9ea471e2444c7932178532ed2d41c6588a82ca3d
Patrick Coleman <Patrick@Patricks-MacBook-Pro.local>
2017-09-07 11:00:34 -0700

    Initial version.

---
+++ Triange/Triange/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Triange
+//
+//  Created by Patrick Coleman on 9/7/17.
+//  Copyright © 2017 Patrick Coleman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
