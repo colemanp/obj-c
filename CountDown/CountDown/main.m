3206baed2f259419934cdfd3cf25509204478ec0
Patrick Coleman <Patrick@Patricks-MacBook-Pro.local>
2017-09-11 14:41:36 -0700

    Add user input.

--- CountDown/CountDown/main.m
+++ CountDown/CountDown/main.m
@@ -7,11 +7,26 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <stdio.h>
+#include <readline/readline.h>
 
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+    
+    printf("Where should I start counting? ");
+
+    
+    const char * buf = readline(NULL);
+    
+    int i = atoi(buf);
+    
+    
+    for(; i >= 0; i -= 3){
+        printf("%d\n", i);
+        
+        if(i % 5 == 0){
+            printf("Found one!\n");
+        }
     }
+    
     return 0;
 }
