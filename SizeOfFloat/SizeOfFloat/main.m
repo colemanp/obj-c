c67a99aa7f7becc4b452e4baec5c3e4e175d057d
Patrick Coleman <Patrick@Patricks-MacBook-Pro.local>
2017-09-11 14:55:42 -0700

    Fix unsaved file

--- SizeOfFloat/SizeOfFloat/main.m
+++ SizeOfFloat/SizeOfFloat/main.m
@@ -9,9 +9,8 @@
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
-    }
+    
+    printf("%lu\n", sizeof(float));
+    
     return 0;
 }
